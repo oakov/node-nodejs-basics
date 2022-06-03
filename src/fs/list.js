e30d23062026c6259e1c0ec6286b6ca036bd2405
OAK <oak110375@gmail.com>
2022-06-03 15:19:30 +0300

    add: fs/list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,16 @@
+import { readdir } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
     // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    try {
+        const files = await readdir(path.join(__dirname, 'files'));
+        console.log(files);
+    } catch (e) {
+        if (e.code === 'ENOENT') throw Error('FS operation failed');
+    }
 };
+
+list();
