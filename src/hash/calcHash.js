8a241b4f6a0b32c88f46a1a97db018c1332200b5
OAK <oak110375@gmail.com>
2022-06-03 20:06:33 +0300

    add: hash/calcHash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,14 @@
+import { createHash } from 'crypto';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { createReadStream } from 'fs';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const hash = createHash('sha256');
+    const input = createReadStream(path.join(__dirname, 'files', 'fileToCalculateHashFor.txt'));
+    input.pipe(hash).setEncoding('hex').pipe(process.stdout);
 };
+
+calculateHash();
