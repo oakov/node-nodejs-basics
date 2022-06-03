1dd38923a0b2fb1545fe288e02229b4466db940b
OAK <oak110375@gmail.com>
2022-06-03 14:00:04 +0300

    add fs/create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,15 @@
+import { writeFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const create = async () => {
     // Write your code here 
-};
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    try {
+        await writeFile(path.join(__dirname, 'files', 'fresh.txt'), 'I am fresh and young', { flag: 'ax' });
+    } catch (e) {
+        if (e.code === 'EEXIST') throw Error('FS operation failed');
+    }
+}
+
+create();
