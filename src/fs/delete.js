2322630e09403f7cdb739c5f0dc5cdd5e6c75739
OAK <oak110375@gmail.com>
2022-06-03 15:12:58 +0300

    add: fs/delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import { rm } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const remove = async () => {
     // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    try {
+        await rm(path.join(__dirname, 'files', 'fileToRemove.txt'));
+    } catch (e) {
+        if (e.code === 'ENOENT') throw Error('FS operation failed');
+    }
 };
+
+remove();
