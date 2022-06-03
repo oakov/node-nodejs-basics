e5ae17dba3e9f22786cc56ec8bcb1a0e6985bafd
OAK <oak110375@gmail.com>
2022-06-03 16:35:43 +0300

    add: cli/args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,13 @@
 export const parseArgs = () => {
     // Write your code here 
+    const args = process.argv.slice(2);
+    if (args.length > 0) {
+        let values = [];
+        for (let i = 0; i < args.length; i = i + 2) {
+            values.push(`${args[i].slice(2)} is ${args[i + 1]}`);
+        }
+        console.log(values.join(', '));
+    }
 };
+
+parseArgs();
