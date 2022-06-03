ab16cb663f484db8dddcaf57fb6269ecbeee3f11
OAK <oak110375@gmail.com>
2022-06-03 16:13:05 +0300

    add: cli/env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
 export const parseEnv = () => {
     // Write your code here 
+    const proceses = process.env;
+    let procArray = [];
+    for (let pr in proceses) {
+        if (pr.match('RSS_')) procArray.push(pr);
+    }
+    console.log(procArray.map((p) => `${p}=${proceses[p]}`).join('; '));
 };
+
+parseEnv();
