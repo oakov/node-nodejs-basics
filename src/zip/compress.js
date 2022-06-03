359d4a97b4c349708557e35ba70ef8cfc411b415
OAK <oak110375@gmail.com>
2022-06-03 21:46:18 +0300

    add: zip/compress & zip/decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,14 @@
+import { createGzip } from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const compress = async () => {
-    // Write your code here 
+    // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const input = createReadStream(path.join(__dirname, 'files', 'fileToCompress.txt'));
+    const output = createWriteStream(path.join(__dirname, 'files', 'archive.gz'));
+    input.pipe(createGzip()).pipe(output);
 };
+
+compress();
