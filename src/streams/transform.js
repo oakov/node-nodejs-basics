454db62a2acb82e5f7c560616f48383982ee8342
OAK <oak.rs.school@gmail.com>
2022-06-03 21:16:20 +0300

    add: streams/transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,15 @@
+import { Transform } from 'stream';
+
 export const transform = async () => {
     // Write your code here 
+    const reverseString = new Transform({
+        transform(chunk, encoding, callback) {
+            this.push(chunk.toString().trim().split("").reverse().join("") + '\n\n');
+            callback();
+        }
+    });
+    process.stdout.write('Write anything. For exit press "Ctrl+C"\n');
+    process.stdin.pipe(reverseString).pipe(process.stdout);
 };
+
+transform();
