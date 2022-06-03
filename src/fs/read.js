81939d2d495c351c27eed38b11965a0ca58cffd6
OAK <oak110375@gmail.com>
2022-06-03 15:25:42 +0300

    add: fs/read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,16 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
     // Write your code here 
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    try {
+        const data = await readFile(path.join(__dirname, 'files', 'fileToRead.txt'), 'utf8');
+        console.log(data);
+    } catch (e) {
+        if (e.code === 'ENOENT') throw Error('FS operation failed');
+    }
 };
+
+read();
