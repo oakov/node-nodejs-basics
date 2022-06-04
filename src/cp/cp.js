113dddc476f64f0ac94ead89cdc4cfc81eae8bb5
OAK <oak110375@gmail.com>
2022-06-05 01:23:54 +0300

    add: cp/cp.js

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,3 +1,13 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 export const spawnChildProcess = async (args) => {
     // Write your code here
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const proc = spawn('node', [path.join(__dirname, 'files', 'script.js'), ...process.argv.slice(2)]);
+    process.stdin.pipe(proc.stdin);
+    proc.stdout.pipe(process.stdout);
 };
+
+spawnChildProcess();
