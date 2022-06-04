903207a4636a20df933f88ce66e4e9575369e8c3
OAK <oak110375@gmail.com>
2022-06-05 00:28:32 +0300

    add: worker threads wt/main wt/worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,3 +1,24 @@
+import { Worker } from 'worker_threads';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+
 export const performCalculations = async () => {
     // Write your code here
-};
+    const __dirname = path.dirname(fileURLToPath(import.meta.url));
+    const promises = [];
+    for (let i = 0; i < cpus().length; i++) {
+        promises.push(new Promise((resolve) => {
+            const worker = new Worker(path.join(__dirname, 'worker.js'), { workerData: 10 + i });
+            worker.on('message', result => {
+                resolve({ status: 'resolved', data: result });
+            });
+            worker.on('error', error => {
+                resolve({ status: 'error', data: null });
+            });
+        }));
+    }
+    console.log(await Promise.all(promises));
+}
+
+performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,6 +1,12 @@
 // n should be received from main thread
+import { parentPort, workerData } from 'worker_threads';
+
 export const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 export const sendResult = () => {
     // This function sends result of nthFibonacci computations to main thread
+    // if (Math.random() < 0.35) throw new Error;
+    parentPort.postMessage(nthFibonacci(workerData));
 };
+
+sendResult();
